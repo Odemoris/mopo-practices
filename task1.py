a7000c802f92e4cc69e656c55f92f8e60eb90773
Odemoris <darya.zhidyaeva@yandex.ru>
2026-03-24 17:35:23 +0500

    Add practice 7 solution

---
+++ task1.py
@@ -0,0 +1,9 @@
+
+num_participants = int(input("Число участников:\n>>> "))
+
+
+for i in range(num_participants):
+    name = input("Введите имя:\n>>> ")
+    print(f"Добро пожаловать, {name}")
+
+print("Групповой чат создан!")
