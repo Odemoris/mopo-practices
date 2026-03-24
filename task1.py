f5859631e70cd8ed7f8dbe34e577e4adb302c12f
Odemoris <darya.zhidyaeva@yandex.ru>
2026-03-24 18:03:22 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,8 +1,5 @@
-
-num_participants = int(input("Число участников:\n>>> "))
-
-
-for i in range(num_participants):
+num = int(input("Число участников:\n>>> "))
+for i in range(num):
     name = input("Введите имя:\n>>> ")
     print(f"Добро пожаловать, {name}")
 
